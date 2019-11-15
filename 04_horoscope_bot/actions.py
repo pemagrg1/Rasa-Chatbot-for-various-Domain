a64f8b61df1687df4f6ba845536c8df8b799b1b5
pemagrg1 <pema.grg1@gmail.com>
2019-11-15 12:57:14 +0545

    config and action code

--- 04_horoscope_bot/actions.py
+++ 04_horoscope_bot/actions.py
@@ -1,27 +1,53 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 #
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+
+from rasa_core_sdk.events import SlotSet
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+#
+class ActionGreet(Action):
+    def name(self):
+        return "action_greet"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message("Hello World!")
-#
-#         return []
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("Hello! Tell me your Horoscope Sign")
+        return []
+
+
+class ActionTodaysHoroscope(Action):
+    def name(self):
+        return "action_todays_horoscope"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_horoscope_sign = tracker.get_slot('horoscope')
+        base_url = "http://horoscope-api.herokuapp.com/horoscope/{day}/{sign}"
+        url = base_url.format(**{'day': "today", 'sign': user_horoscope_sign})
+        # http://horoscope-api.herokuapp.com/horoscope/today/capricorn
+        res = requests.get(url, 'html.parser')
+        todays_horoscope = res.json()['horoscope']
+        response = "Your today's horoscope:\n{}".format(todays_horoscope)
+        dispatcher.utter_message(response)
+        return [SlotSet("horoscope", user_horoscope_sign)]
+
+
+class ActionGoodbye(Action):
+    def name(self):
+        return "action_goodbye"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("Bye ! c u soon")
+        return []
